05dfa75dcfe59468c5eb8925fe325d5f292890a9
levmorozov <mail@levmorozov.ru>
2020-03-19 03:00:46 +0300

    strict_types and s3 ver

--- src/Resizer.php
+++ src/Resizer.php
@@ -1,8 +1,7 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace levmorozov\phpresizer;
 
-use Jcupitt\Vips\Access;
 use Jcupitt\Vips\BlendMode;
 use Jcupitt\Vips\Image;
 use Jcupitt\Vips\Interesting;
