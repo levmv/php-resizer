4346594b58037698fb78fdae3d6d60df7f48cd51
levmorozov <mail@levmorozov.ru>
2020-07-21 09:41:45 +0300

    Add helpers for custom usage cases

--- src/Url.php
+++ src/Url.php
@@ -127,4 +127,40 @@ class Url
 
         return \implode(',', $parts);
     }
+
+    /**
+     * Return short base64 encoded hash of $string + $secret
+     *
+     * @param string $string
+     * @param string $secret
+     * @param int    $offset
+     * @param int    $size
+     * @return string
+     */
+    public static function shortHash(string $string, string $secret, int $offset = 8, int $size = 3) : string
+    {
+        \assert($offset < 16 && $size < 16);
+        return \rtrim(\strtr(\base64_encode(\substr(\md5($string.$secret, true), $offset, $size)), '+/', '-_'), '=');
+    }
+
+
+    /**
+     * Parses URI like "sign/url", checks sign and returns url if everything ok
+     * othewise null
+     *
+     * @param $secret
+     * @return string|null
+     */
+    public static function parseUri(string $secret) : ?string
+    {
+        $uri = \ltrim($_SERVER['REQUEST_URI'], '/');
+        [$sign, $url] = \explode('/', $uri, 2);
+
+        if($sign !== self::shortHash($url, $secret)) {
+            \http_response_code(404);
+            return null;
+        }
+
+        return $url;
+    }
 }
